1a9fdfc8f5d4ef9acf8fb9258f063da8e51615bf
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-08-22 16:10:45 +0600

    ci: example music page

---
+++ src/app/examples/music/data/playlists.ts
@@ -0,0 +1,16 @@
+export const playlists = [
+  'Recently Added',
+  'Recently Played',
+  'Top Songs',
+  'Top Albums',
+  'Top Artists',
+  'Logic Discography',
+  'Bedtime Beats',
+  'Feeling Happy',
+  'I miss Y2K Pop',
+  'Runtober',
+  'Mellow Days',
+  'Eminem Essentials',
+];
+
+export type Playlist = (typeof playlists)[number];
