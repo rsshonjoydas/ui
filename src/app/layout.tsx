79348585a7c460ff245cef540501dcbe99ed6304
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-08-22 16:04:29 +0600

    ci: home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
+import { Analytics } from '@/components/analytics';
+import { ThemeProvider } from '@/components/providers';
+import { SiteFooter } from '@/components/site-footer';
+import { SiteHeader } from '@/components/site-header';
+import { TailwindIndicator } from '@/components/tailwind-indicator';
+import { ThemeSwitcher } from '@/components/theme-switcher';
 import env from '@/config/env';
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/lib/fonts';
 import { absoluteUrl, cn } from '@/lib/utils';
+import { Toaster as DefaultToaster } from '@/registry/default/ui/toaster';
+import { Toaster as NewYorkToaster } from '@/registry/new-york/ui/toaster';
 import '@/styles/index.scss';
 import type { Metadata } from 'next';
 
@@ -61,10 +69,29 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en'>
       <head />
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        {children}
+      <body
+        suppressHydrationWarning
+        className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className='relative flex flex-col min-h-screen'>
+            <SiteHeader />
+            <div className='flex-1'>{children}</div>
+            <SiteFooter />
+          </div>
+          <TailwindIndicator />
+        </ThemeProvider>
+        <ThemeSwitcher />
+        <Analytics />
+        <NewYorkToaster />
+        <DefaultToaster />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,71 @@
-export default function Home() {
+import { ArrowRightIcon } from '@radix-ui/react-icons';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { ExamplesNav } from '@/components/examples-nav';
+import { Icons } from '@/components/icons';
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/page-header';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/registry/new-york/ui/button';
+import { Separator } from '@/registry/new-york/ui/separator';
+import DashboardPage from './examples/dashboard/page';
+
+export default function IndexPage() {
   return (
-    <main>
-      <h1>Redolence</h1>
-    </main>
+    <div className='container relative'>
+      <PageHeader className='pb-8'>
+        <Link
+          href='/docs/changelog'
+          className='inline-flex items-center px-3 py-1 text-sm font-medium rounded-lg bg-muted'
+        >
+          🎉 <Separator className='h-4 mx-2' orientation='vertical' />{' '}
+          <span className='sm:hidden'>Style, a new CLI and more.</span>
+          <span className='hidden sm:inline'>Introducing Style, a new CLI and more.</span>
+          <ArrowRightIcon className='w-4 h-4 ml-1' />
+        </Link>
+        <PageHeaderHeading>Build your component library.</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your apps. Accessible.
+          Customizable. Open Source.
+        </PageHeaderDescription>
+        <div className='flex items-center w-full pt-4 pb-8 space-x-4 md:pb-10'>
+          <Link href='/docs' className={cn(buttonVariants())}>
+            Get Started
+          </Link>
+          <Link
+            target='_blank'
+            rel='noreferrer'
+            href={siteConfig.links.github}
+            className={cn(buttonVariants({ variant: 'outline' }))}
+          >
+            <Icons.GitHub className='w-4 h-4 mr-2' />
+            GitHub
+          </Link>
+        </div>
+      </PageHeader>
+      <ExamplesNav className='[&>a:first-child]:text-primary' />
+      <section className='space-y-8 overflow-hidden border-2 rounded-lg border-primary dark:border-muted md:hidden'>
+        <Image
+          src='/examples/dashboard-light.png'
+          width={1280}
+          height={866}
+          alt='Dashboard'
+          className='block dark:hidden'
+        />
+        <Image
+          src='/examples/dashboard-dark.png'
+          width={1280}
+          height={866}
+          alt='Dashboard'
+          className='hidden dark:block'
+        />
+      </section>
+      <section className='hidden md:block'>
+        <div className='overflow-hidden border rounded-lg shadow bg-background'>
+          <DashboardPage />
+        </div>
+      </section>
+    </div>
   );
 }
